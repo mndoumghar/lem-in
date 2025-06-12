ad05e967447ef8eab40e5db3bb3a8ad33028b9a3
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-12 14:29:19 +0100

    update page main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"lemin/internal/dfs"
+	"lemin/internal/movement"
+
 	//"lemin/internal/movement"
 
 	"lemin/internal/ants"
-	"os"
 )
-
-
-
-
 
 func main() {
 	if len(os.Args) != 2 {
@@ -20,26 +18,19 @@ func main() {
 		return
 	}
 	nmla := ants.ReadFile(os.Args[1])
-	
+
 	if nmla.Err != nil {
 		fmt.Printf("Error: %v\n", nmla.Err)
 		return
 	}
-	fmt.Println(nmla.Start, nmla.End, nmla.Links)
-
 
 	paths := dfs.FindPaths(nmla.Start, nmla.End, nmla.Links)
 
-
 	paths = dfs.RemoveDuplicatePaths(paths)
-	fmt.Println(nmla)
-	fmt.Println("tr9an  : ",paths)
 
 	if len(paths) == 0 {
 		fmt.Println("ERROR: invalid data format, no path found")
 		return
 	}
-	/////fmt.Println("nmla : ", nmla.AntNum)
-//	movement.SimulateAntMovement(paths, nmla.AntNum)
-
+	movement.SimulateAntMovement(paths, nmla.AntNum)
 }
